3e791264743a1b3df596b30c2640c38a4a6dd92d
Shirel Zohar <shirelrn01@gmail.com>
2024-11-12 11:47:04 +0200

    recipe schema

---
+++ src/app/types/recipeSchema.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+const Recipe = z.object({
+    name: z.string().min(2, "Name must be at least 2 characters"),
+    category: z.string(),
+    image: z.string().url(),
+    ingredients: z.array(z.string()).max(20,  "Array can contain a maximum of 20 items"),
+    instructions: z.string().min(2, "Instructions must be at least 2 characters").max(100, "Instructions must be at least 2 characters"),
+    isFavorite: z.boolean()
+
+  });
+
+  export type RecipeType = z.infer<typeof Recipe>;
+export default Recipe
